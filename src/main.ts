3633f4220fa8602bd2e6e35e3ccc7b7f1584a183
thaophamhetxat <thaophamhetxat@gmail.com>
2021-09-17 10:31:32 +0700

    done

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,24 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
+function isPrime(number: number) {
+  let isPrime = true;
+  if (number < 2) {
+    isPrime = false;
+  }else if(number>2){
+    for(let i=2;i<Math.sqrt(number);i++){
+      if(number%i==0){
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  return isPrime;
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let arr = [1, 5, 9, 2, 6, 15, 19, 35, 51, 53];
+let sum = 0;
+for (let number of arr){
+  if(isPrime(number)){
+    sum +=number;
+  }
+}
+
+console.log("tong so nguyen to trong mang:" +sum);
